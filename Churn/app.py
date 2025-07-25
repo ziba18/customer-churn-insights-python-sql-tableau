43cbe0328f0c4c9868abc6d486498c2df5250778
Ziba Zamani <51383813+ziba18@users.noreply.github.com>
2025-07-25 19:54:36 +0400

    Add files via upload

---
+++ Churn/app.py
@@ -0,0 +1,7 @@
+# Gender -> 1 Female  0 Male
+# Churn   -> 1 Yes   0 No
+# Scaler is exported as scaler.pkl
+# Model is exported as model.pkl
+# Order of the X -> 'Age', 'Gender', 'Tenure', 'MonthlyCharges'
+
+import streamlit as st
