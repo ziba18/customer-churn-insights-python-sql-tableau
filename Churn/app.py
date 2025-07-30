f69e4af16a3a7e60a457ac8a8962add74e29fbf2
Ziba Zamani <51383813+ziba18@users.noreply.github.com>
2025-07-30 23:28:29 +0400

    Update app.py

--- Churn/app.py
+++ Churn/app.py
@@ -1,5 +1,5 @@
 # Gender -> 1 Female  0 Male
-# Churn   -> 1 Yes   0 No
+# Churn  -> 1 Yes     0 No
 # Scaler is exported as scaler.pkl
 # Model is exported as model.pkl
 # Order of the X -> 'Age', 'Gender', 'Tenure', 'MonthlyCharge'
